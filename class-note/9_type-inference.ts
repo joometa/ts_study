36c4cfcfadff372ed825dbed0df6ac5c7a9d869d
JeongJoo-Lee <jjlee1623@gmail.com>
2021-11-18 00:01:55 +0900

    타입추론(3)

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -1,4 +1,4 @@
-// 타입 추론
+// 타입 추론(1)
 
 var a; // any 추론
 var b = 10; // 할당시 할당된 값의 타입으로 추론
@@ -10,3 +10,40 @@ function getD(d = 10) {
   var e = "hi";
   return d + e;
 }
+
+// 타입 추론 (2)
+interface Updown<T> {
+  value: T;
+  title: string;
+}
+
+// interface에서 value값을 제네릭과 연결시켰기 때문에 아래 shoppingItem에서 value의 타입이 string인것을 추론할 수 있음
+// 하지만 title의 타입은 이미 interface에서 string으로 선언되었기 때문에 고정값이라 변하지 않음
+var shoppingItem: Updown<string> = {
+  value: "hi",
+  title: "hello",
+};
+
+// 타입 추론(3)
+interface Updown2<T> {
+  value: T;
+  title: string;
+}
+interface DetailedUpdown<K> extends Updown2<K> {
+  description: string;
+  tag: K;
+}
+// interface DetailedUpdown<K> extends Updown2<K>{  //암묵적으로 위 extends는 이런식으로 동작할 것임
+//   description: string;
+//   tag: K;
+//   value: K;
+//   title: string;
+// }
+
+// extends한 타입에 제네릭을 number로 지정하니 제네릭과 연결된 키값의 타입들이 number로 자동적으로 추론되어 할당되는것을 알 수 있다.
+var detailedItem: DetailedUpdown<number> = {
+  title: "abc",
+  description: "ab",
+  value: 1111,
+  tag: 2222,
+};
