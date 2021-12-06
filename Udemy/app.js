c3f75c4fb6cb0165b3409e5eb3eb8f1c65f2e155
JeongJoo-Lee <jjlee1623@gmail.com>
2021-12-06 22:13:43 +0900

    Udemy start

---
+++ Udemy/app.js
@@ -0,0 +1,5 @@
+var person = {
+    name: "max",
+    age: 30
+};
+console.log(person.nickname);
---
+++ Udemy/app.ts
@@ -0,0 +1,6 @@
+const person: { name: string; age: number } = {
+  name: "max",
+  age: 30,
+};
+
+console.log(person.nickname);
