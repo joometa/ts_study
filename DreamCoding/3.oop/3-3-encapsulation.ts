36ff1ffb38b8b3123cb99cd10b06dafe5bd410b4
JeongJoo-Lee <jjlee1623@gmail.com>
2022-01-10 00:18:21 +0900

    캡슐화 : public,private,protected 활용

---
+++ DreamCoding/3.oop/3-3-encapsulation.ts
@@ -0,0 +1,50 @@
+// 캡슐화
+
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  // public
+  // private
+  // protected
+  class CoffeeMaker {
+    // 따로 선언안하면 모두 public 이다.
+    private static BEANS_GRAMM_PER_SHOT = 7;
+    private coffeeBeans: number = 0;
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeCoffeeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("커피콩은 0개 이상이어야 합니다.");
+      }
+
+      this.coffeeBeans += beans;
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+
+  const maker = new CoffeeMaker(32);
+  // maker.coffeeBeans = 3;
+  // maker.coffeeBeans = -34; -> 외부에서 지정하면 말도안되는 값을 받게됨(invalid) 그래서 숨겨줄 필요가 있다.(private)
+  maker.fillCoffeeBeans(12);
+  // private을 이용해 함수로 커피빈을 설정할수밖에없게 만들었음. 보안성이 더 높아진다.
+}
